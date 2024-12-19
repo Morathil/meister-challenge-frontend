a0aa728afe70818a3c51252f8a9a98e004f0b503
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-19 21:04:32 +0100

    Add switch to change between list and grid view and dummy data for project switch

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,6 +5,8 @@ import Toolbar from '@mui/material/Toolbar'
 import Drawer from '@mui/material/Drawer'
 import Divider from '@mui/material/Divider'
 import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Switch from '@mui/material/Switch'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -14,11 +16,19 @@ import Typography from '@mui/material/Typography'
 import TaskGridLayout from 'views/App/GridLayout'
 import TaskListLayout from 'views/App/ListLayout'
 
-const tasks = ['t1', 't2', 't3', 't4', 't5']
+const baseTasks = ['t1', 't2', 't3', 't4', 't5']
+const projects = ['Project1', 'Project2', 'Project3']
+const tasksByProject: { [key: string]: string[]} = projects.reduce((r: any, p) => {
+  r[p] = baseTasks.map((t) => `${p}-${t}`)
+  return r
+}, {})
 
-// App
-export default () => {
-  const [useGridLayout, setUseGridLayout] = useState(true);
+export default function App () {
+  const [useListLayout, setUseListLayout] = useState(true);
+  const [currentProject, setCurrentProject] = useState(projects[0]);
+
+  const currentTasks = tasksByProject[currentProject]
+
   const drawerWidth = 180
 
   return (
@@ -26,7 +36,14 @@ export default () => {
       <CssBaseline />
       <AppBar position='fixed' sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <Typography variant='h6'>Meister Challenge</Typography>
+          <Typography variant='h6' style={{ flexGrow: 1 }}>Meister Challenge</Typography>
+          <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+            <Typography>Grid</Typography>
+            <Switch defaultChecked color='default' onChange={(event) => {
+              setUseListLayout(event.target.checked)
+            }} />
+            <Typography>List</Typography>
+          </Stack>    
         </Toolbar>
       </AppBar>
       <Drawer
@@ -40,9 +57,9 @@ export default () => {
         <Toolbar />
         <Divider />          
         <List>
-          {['Project1', 'Project2', 'Project3'].map((text, index) => (
+          {projects.map((text) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
+              <ListItemButton onClick={() => setCurrentProject(text)}>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
@@ -51,8 +68,7 @@ export default () => {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <TaskGridLayout tasks={tasks} />
-        <TaskListLayout tasks={tasks} />
+        {useListLayout ? <TaskListLayout tasks={currentTasks} /> : <TaskGridLayout tasks={currentTasks} />}
       </Box>
     </Box>
   )
