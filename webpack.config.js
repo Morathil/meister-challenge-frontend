1810a540f982bd8dff17cc99416f5f669b154861
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-19 20:53:04 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const IS_RELEASE = !!process.env.RELEASE
+
+let entries = {
+  app: path.resolve(__dirname, 'src/index.tsx')
+}
+
+let plugins = [
+  // new ForkTsCheckerWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/index.html'),
+    filename: 'index.html',
+    inject: 'body',
+    chunks: ['app']
+  })
+]
+
+module.exports = {
+  mode: IS_RELEASE ? 'production' : 'development',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+  entry: entries,
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+      views: path.resolve(__dirname, 'src/views/'),
+    }
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    chunkFilename: '[id].js',
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true
+        }
+      },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader'
+      //   }
+      // },
+    ]
+  },
+  plugins: plugins
+}
