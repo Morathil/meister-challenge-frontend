cf9e292f6e3e2a946fc3525de1ff592eaf134d75
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-22 16:57:25 +0100

    Add tasks by project id gql query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,16 +1,19 @@
 import { gql } from '@apollo/client';
 
-export const FETCH_PROJECTS = {
-    query: gql`
-      query GetProjects {
-        projects {
-          id
-          name
-          tasks {
-            id
-            name
-          }
-        }
-      }
-    `,
+export const FETCH_PROJECTS = gql`
+  query GetProjects {
+    projects {
+      id
+      name
+    }
   }
+`
+
+export const FETCH_TASKS_BY_PROJECT_ID = gql`
+  query GetTasksByProjectId($projectId: ID!) {
+    tasks(projectId: $projectId) {
+      id
+      name
+    }
+  }
+`
