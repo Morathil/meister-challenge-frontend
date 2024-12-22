00c85a1ae6caffdc900debabd38569c270eb7aca
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-22 16:58:26 +0100

    Add apollo client and apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
-
 import App from 'views/App'
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from 'src/services/apollo-client'
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 )
